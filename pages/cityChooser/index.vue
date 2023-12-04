ebf8b32115be5b04cddee31c0561ba8b4c5834c6
HugoSbl <hugo.sablot@gmail.com>
2023-12-04 11:18:54 +0100

    first commit

---
+++ pages/cityChooser/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h1>Welcome to the City Chooser</h1>
+    <p>Choose a city from the list below:</p>
+    <ul>
+      <li v-for="city in cities" :key="city.id">{{ city.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLink to="/cityChooser">Go to the City Chooser</NuxtLink>
+  <Button variant="destructive">Hello</Button>
+</template>
+<script setup>
+
+</script>
+
